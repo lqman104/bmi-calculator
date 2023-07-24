674cfdb37795c84350a44ec8c257a6032cbe00a1
Luqman <lqman104@gmail.com>
2023-07-24 11:19:11 +0700

    feat: create ui skeleton

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,7 @@
+import 'package:bmi_calculator/my_color.dart';
 import 'package:flutter/material.dart';
+
+const bottomContainerHeight = 80.0;
 
 class InputPage extends StatefulWidget {
   @override
@@ -12,8 +15,80 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Container(
-        child: Text('Body Text'),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(
+                      child: ReusableCard(
+                        color: MyColor.cardBackground,
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                      child: ReusableCard(
+                        color: MyColor.cardBackground,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 16),
+              Expanded(
+                child: ReusableCard(
+                  color: MyColor.cardBackground,
+                ),
+              ),
+              SizedBox(height: 16),
+              Expanded(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(
+                      child: ReusableCard(
+                        color: MyColor.cardBackground,
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                      child: ReusableCard(
+                        color: MyColor.cardBackground,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: bottomContainerHeight,
+                margin: EdgeInsets.only(top: 10),
+                color: MyColor.secondary,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color color;
+
+  ReusableCard({required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(15),
       ),
     );
   }
--- lib/my_color.dart
+++ lib/my_color.dart
@@ -1,6 +1,11 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 class MyColor {
   static const Color primary = Color(0xFF0A0D22);
+  static const Color secondary = Color(0xFFEB5555);
   static const Color cardBackground = Color(0xFF1D1E33);
+  static const Color disable = Color(0xFF8D8E98);
+  static const Color enable = Colors.white;
 }

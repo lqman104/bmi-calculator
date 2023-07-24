d5a9f1c33a52c382024936f0aa54236df515ad85
Luqman <lqman104@gmail.com>
2023-07-25 06:30:06 +0700

    style: reindent

--- lib/typography.dart
+++ lib/typography.dart
@@ -1,13 +1,7 @@
 import 'package:bmi_calculator/my_color.dart';
 import 'package:flutter/cupertino.dart';
 
-const kLabelTextStyle = TextStyle(
-  fontSize: 18,
-  color: MyColor.disable
-);
+const kLabelTextStyle = TextStyle(fontSize: 18, color: MyColor.black300);
 
 const kTitleTextStyle = TextStyle(
-  fontSize: 50,
-  color: MyColor.disable,
-  fontWeight: FontWeight.w900
-);
+    fontSize: 50, color: MyColor.black300, fontWeight: FontWeight.w900);

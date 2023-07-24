ab1d49dbc18a6fa176f189e9978a00a8ef9a27a8
Luqman <lqman104@gmail.com>
2023-07-25 06:29:45 +0700

    refactor: color naming

--- lib/my_color.dart
+++ lib/my_color.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
 abstract class MyColor {
-  static const Color primary = Color(0xFF0A0D22);
+  static const Color primary = black900;
   static const Color secondary = Color(0xFFEB5555);
-  static const Color cardBackground = Color(0xFF1D1E33);
-  static const Color disable = Color(0xFF8D8E98);
-  static const Color enable = Colors.white;
+
+  static const Color black900 = Color(0xFF0A0D22);
+  static const Color black800 = Color(0xFF111428);
+  static const Color black700 = Color(0xFF1D1E33);
+  static const Color black600 = Color(0xFF1C1F32);
+  static const Color black300 = Color(0xFF8D8E98);
+
 }
